3e7cc25aeca72f4d94a23f68755f79a8b60d2d19
RAMJAN <niloyahmadramjan@gmail.com>
2026-05-06 17:12:06 +0800

    learn(ts): add  oop - guard type  examples

---
+++ src/oop/typeguard.ts
@@ -0,0 +1,45 @@
+// type of guard
+// type guard typeof and in
+// access modifiets
+// getter and setter
+// static in typescript
+
+type Alphaneumeric = number | string;
+
+const add = (num1: Alphaneumeric, num2: Alphaneumeric) => {
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    return num1 + num2;
+  } else {
+    return num1.toString() + num2.toString();
+  }
+};
+
+console.log(add(2, 2)); // 4
+
+console.log(add(2, "7")); // 27
+
+console.log(add("2", 3)); // 23
+
+console.log(add("5", "2")); //52
+
+// in guard
+
+type NormalUser = {
+  name: string;
+};
+
+type AdminUser = {
+  name: string;
+  role: "Admin";
+};
+
+
+const getUserInfo = (user: NormalUser | AdminUser)=>{
+    if ("role" in user ){
+        console.log(`${user.name}, and his role is : ${user.role}`)
+    }else{
+        console.log(user.name)
+    }
+}
+
+getUserInfo({ name: "Ramjan"});
